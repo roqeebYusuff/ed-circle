cd7962674ddb4a8bab60d12e5a9435e4d9cbc735
roqeebYusuff <roqeebyusuff001@gmail.com>
2022-12-31 14:18:23 +0100

    :memo: Footer adj

--- components/Footer.js
+++ components/Footer.js
@@ -14,40 +14,35 @@ const Footer = () => {
           </div>
           <div className="flex flex-col gap-2">
             <p className="text-gray-500 text-xl font-semibold">Product</p>
-            <p className="text-lg  font-medium">Overview</p>
-            <p className="text-lg  font-medium">Features</p>
-            <p className="text-lg  font-medium">Solution</p>
-            <p className="text-lg  font-medium">Tutorials</p>
-            <p className="text-lg  font-medium">Pricing</p>
+            <p className="text-sm  font-medium">Overview</p>
+            <p className="text-sm  font-medium">Features</p>
+            <p className="text-sm  font-medium">Solution</p>
+            <p className="text-sm  font-medium">Tutorials</p>
+            <p className="text-sm  font-medium">Pricing</p>
           </div>
           <div className="flex flex-col gap-2">
             <p className="text-gray-500 text-xl font-semibold">Company</p>
-            <p className="text-lg  font-medium">About Us</p>
-            <p className="text-lg  font-medium">Career</p>
-            <div className="flex gap-1">
-              <p className="text-lg  font-medium">press</p>
-              <p className="text-xs flex justify-center items-center  font-semibold text-black bg-white px-2 py-1 rounded-full">
-                new
-              </p>
-            </div>
+            <p className="text-sm  font-medium">About Us</p>
+            <p className="text-sm  font-medium">Career</p>
+            <p className="text-sm  font-medium">Press</p>
 
-            <p className="text-lg  font-medium">News</p>
+            <p className="text-sm  font-medium">News</p>
           </div>
           <div className="flex flex-col gap-2">
             <p className="text-gray-500 text-xl font-semibold">Social</p>
-            <p className="text-lg  font-medium">Facebook</p>
-            <p className="text-lg  font-medium">Youtube</p>
+            <p className="text-sm  font-medium">Facebook</p>
+            <p className="text-sm  font-medium">Youtube</p>
 
-            <p className="text-lg  font-medium">Twitter</p>
-            <p className="text-lg  font-medium">Linkedin</p>
+            <p className="text-sm  font-medium">Twitter</p>
+            <p className="text-sm  font-medium">Linkedin</p>
           </div>
           <div className="flex flex-col gap-2">
             <p className="text-gray-500 text-xl font-semibold">Legal</p>
-            <p className="text-lg  font-medium">Terms</p>
-            <p className="text-lg  font-medium">Privacy</p>
+            <p className="text-sm  font-medium">Terms</p>
+            <p className="text-sm  font-medium">Privacy</p>
 
-            <p className="text-lg  font-medium">Cookies</p>
-            <p className="text-lg  font-medium">Contact</p>
+            <p className="text-sm  font-medium">Cookies</p>
+            <p className="text-sm  font-medium">Contact</p>
           </div>
         </div>
         <div className="flex justify-between items-center pt-20 pb-5">
